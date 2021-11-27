40b4aef1581a397c51b2eae121e9132166e64a9b
usatyo <yu0424.math8128@gmail.com>
2021-11-28 01:50:03 +0900

    選択マス、コママスの取得

--- frontend/src/components/Cell.js
+++ frontend/src/components/Cell.js
@@ -2,23 +2,54 @@ import style from '../styles/Cell.module.scss'
 import { useState, useEffect } from 'react'
 
 const playSize = 10;
-const YELLOW = 0
-const RED = 1
-const BLUE = 2
-const LIGHTRED = 3
-const LIGHTBLUE = 4
+const RED = 0
+const BLUE = 1
+const LIGHTRED = 2
+const LIGHTBLUE = 3
+const YELLOW = 4
 
 const undefArray = new Array(playSize)
-const undefFill = new Array(playSize)
+const undefCellInfo = new Array(playSize)
 undefArray.fill(undefined)
-undefFill.fill(undefArray)
+undefCellInfo.fill(undefArray)
+
+const undefPlace = { x: undefined, y: undefined }
+const undefPlaces = [undefPlace, undefPlace]
+const undefCurrntPlace = [undefPlaces, undefPlaces]
 
 const Cell = () => {
-  const [cellInfo, setCellInfo] = useState(undefFill)
-  const [selectedX, setSelectedX] = useState(undefined)
-  const [selectedY, setSelectedY] = useState(undefined)
+  // 各マスの色
+  const [cellInfo, setCellInfo] = useState(undefCellInfo)
+  // 選択されているコマがあるマス
+  const [selected, setSelected] = useState(undefPlace)
+  // コマのあるマス（１つ目のインデックスで色、２つ目のインデックスでどちらかを指定）
+  const [currentPlace, setCurrentPlace] = useState(undefCurrntPlace)
+  // どちらの番か
   const [isRed, setIsRed] = useState(true)
   let changedInfo = cellInfo
+
+  const checkWinner = (color) => {
+    const conditionArray = changedInfo.map((array, x) => {
+      return (
+        array.map((value, y) => {
+          return (value === undefined)
+        })
+      )
+    })
+  }
+
+  const endProcess = () => {
+    const redWin = checkWinner(RED)
+    const blueWin = checkWinner(BLUE)
+    if (redWin && blueWin) {
+
+    } else if (redWin) {
+
+    } else if (blueWin) {
+
+    }
+    return
+  }
 
   const colorCell = (targetX, targetY, color) => {
     changedInfo = changedInfo.map((array, x) => {
@@ -31,15 +62,17 @@ const Cell = () => {
       )
     })
     setCellInfo(changedInfo)
+    if(color !== RED && color !== BLUE) return
+    let moved = 0
+    if (currentPlace[color][1].x === targetX && currentPlace[color][1].y === targetY) {
+      moved = 1
+    }
+    let changedPlace = currentPlace.slice()
+    changedPlace[color][moved] = { x: targetX, y: targetY }
+    setCurrentPlace(changedPlace)
+    return
   }
-      
-  useEffect(() => {
-    colorCell(0, 0, RED)
-    colorCell(0, playSize - 1, BLUE)
-    colorCell(playSize - 1, 0, BLUE)
-    colorCell(playSize - 1, playSize - 1, RED)
-  }, [])
-      
+
   const colorYellowCell = (targetX, targetY) => {
     changedInfo = changedInfo.map((array, x) => {
       return (
@@ -53,22 +86,32 @@ const Cell = () => {
       )
     })
     setCellInfo(changedInfo)
-    setSelectedX(targetX)
-    setSelectedY(targetY)
+    setSelected({ x: targetX, y: targetY })
+    return
   }
 
-  const handleClick = (clickedX, clickedY) => {
+  useEffect(() => {
+    colorCell(0, 0, RED)
+    colorCell(0, playSize - 1, BLUE)
+    colorCell(playSize - 1, 0, BLUE)
+    colorCell(playSize - 1, playSize - 1, RED)
+    setCurrentPlace([
+      [{ x: 0, y: 0 }, { x: playSize - 1, y: playSize - 1 }],
+      [{ x: 0, y: playSize - 1 }, { x: playSize - 1, y: 0 }]
+    ])
+  }, [])
 
+  const handleClick = (clickedX, clickedY) => {
     if (changedInfo[clickedX][clickedY] === (isRed ? RED : BLUE)) {
       colorYellowCell(clickedX, clickedY)
       return
     }
-
     if (changedInfo[clickedX][clickedY] !== YELLOW) return
-
     colorCell(clickedX, clickedY, isRed ? RED : BLUE)
-    colorCell(selectedX, selectedY, isRed ? LIGHTRED : LIGHTBLUE)
+    colorCell(selected.x, selected.y, isRed ? LIGHTRED : LIGHTBLUE)
+    endProcess()
     setIsRed(!isRed)
+    return
   }
 
   return (
@@ -81,7 +124,7 @@ const Cell = () => {
                 type='button'
                 key={x + '_' + y}
                 className={`${style.cell} 
-                  ${value === undefined ? style.unColored : (
+                  ${value === undefined ? style.uncolored : (
                     value === YELLOW ? style.yellow : (
                       value === RED ? style.red : (
                         value === BLUE ? style.blue : (
